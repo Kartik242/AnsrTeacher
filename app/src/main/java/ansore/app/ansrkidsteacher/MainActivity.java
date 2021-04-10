55f1879df6b7d038dd599868f3804d2387995048
Kartik242 <kartikeye.pandey35@gmail.com>
2021-04-10 13:07:45 +0530

    Initial commit

---
+++ app/src/main/java/ansore/app/ansrkidsteacher/MainActivity.java
@@ -0,0 +1,14 @@
+package ansore.app.ansrkidsteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
