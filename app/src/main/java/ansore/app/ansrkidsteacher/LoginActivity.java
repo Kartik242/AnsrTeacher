e106d284b3ff8673bccf6dc61d8319147bde0a31
Kartik242 <kartikeye.pandey35@gmail.com>
2021-04-12 22:46:50 +0530

    login

--- app/src/main/java/ansore/app/ansrkidsteacher/LoginActivity.java
+++ app/src/main/java/ansore/app/ansrkidsteacher/LoginActivity.java
@@ -3,10 +3,17 @@ package ansore.app.ansrkidsteacher;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -26,7 +33,9 @@ public class LoginActivity extends AppCompatActivity {
 
 
     private TextInputEditText t_email,t_password;
-    private String email, password;
+    Button loginn;
+    SharedPreferences sharedPreferences;
+    CheckBox loginState;
 
     private String URL = "http://ansrkids.in/sign_in.php";
 
@@ -35,67 +44,129 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        sharedPreferences = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
+
         t_email = findViewById(R.id.t_email);
         t_password = findViewById(R.id.t_password);
+        loginn = findViewById(R.id.login_btn);
+        loginState = findViewById(R.id.checkbox);
+
+        
+        
+        loginn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = t_email.getText().toString();
+                String password = t_password.getText().toString();
+                login(email,password);
+                
+            }
+        });
+
+        String loginStatus = sharedPreferences.getString(getResources().getString(R.string.preLoginStatus),"");
+        if (loginStatus.equals("loggedin")){
+            startActivity(new Intent(LoginActivity.this,homeActivity.class));
+            finish();
+
+        }
 
 
 
-//        userLogin( t_email.getText().toString(), t_password.getText().toString());
+
+
     }
 
-    private void userLogin(String email, String password) {
+    private void login(final String t_email, final String  t_password) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setTitle("Signing In");
+        progressDialog.show();
+        StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("Login Success")){
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    if (loginState.isChecked()){
+                        editor.putString(getResources().getString(R.string.preLoginStatus),"loggedin");
 
-        if(email.equals("") || password.equals("")){
-            Toast.makeText(LoginActivity.this, "Username or password must be filled", Toast.LENGTH_LONG).show();
-            return;
-        }
-        if(email.length() <= 1 || password.length() <= 1){
-            Toast.makeText(LoginActivity.this, "Username or password length must be greater than one", Toast.LENGTH_LONG).show();
-            return;
-        }
-    }
+                    }else{
+                        editor.putString(getResources().getString(R.string.preLoginStatus),"loggedout");
 
-    public void login_Funs(View view) {
 
-        email = t_email.getText().toString().trim();
-        password = t_password.getText().toString().trim();
-
-        if (!email.equals("")&&!password.equals("")){
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    if (response.equals("success")) {
-                        makeToast("success");
-                        Intent intent = new Intent(LoginActivity.this, homeActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else if (response.equals("failure")) {
-                        makeToast("failure");
                     }
+                    editor.apply();
+
+                    startActivity(new Intent(LoginActivity.this,homeActivity.class));
+                    finish();
+                }else{
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    makeToast(error.toString().trim());
-                }
-            }){
-                @Nullable
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String, String> data = new HashMap<>();
-                    data.put("email",email);
-                    data.put("password",password);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                    data.put("email",t_email);
+                    data.put("password",t_password);
                     return data;
-                }
-            };
-            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-            requestQueue.add(stringRequest);
-        }else{
-            makeToast("Fields can not be empty.");
-        }
+
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(request);
     }
 
-    private void makeToast(String string) {
-        Toast.makeText(LoginActivity.this, string, Toast.LENGTH_LONG).show();
-    }
+
+//    public void login_Funs(View view) {
+//
+//        email = t_email.getText().toString().trim();
+//        password = t_password.getText().toString().trim();
+//
+//        if (!email.equals("")&&!password.equals("")){
+//            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    if (response.equals("1")) {
+//                        makeToast("success");
+//                        Intent intent = new Intent(LoginActivity.this, homeActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    } else  {
+//                        Toast.makeText(LoginActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+//                        Log.e("tag",response);
+//
+//                    }
+//                }
+//            }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    makeToast(error.toString().trim());
+//                }
+//            }){
+//                @Override
+//                protected Map<String, String> getParams() throws AuthFailureError {
+//                    Map<String, String> data = new HashMap<>();
+//                    data.put("email",email);
+//                    data.put("password",password);
+//                    return data;
+//                }
+//            };
+//            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+//            requestQueue.add(stringRequest);
+//        }else{
+//            makeToast("Fields can not be empty.");
+//        }
+//    }
+
+   
 }
--- app/src/main/java/ansore/app/ansrkidsteacher/homeActivity.java
+++ app/src/main/java/ansore/app/ansrkidsteacher/homeActivity.java
@@ -2,13 +2,33 @@ package ansore.app.ansrkidsteacher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homeActivity extends AppCompatActivity {
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        SharedPreferences sharedPreferences = getSharedPreferences("UserInfo",MODE_PRIVATE);
+
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(getResources().getString(R.string.preLoginStatus),"loogedout");
+                editor.apply();
+                startActivity(new Intent(homeActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
